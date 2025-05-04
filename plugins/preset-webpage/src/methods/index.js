69ed581491a11eccba0d0d61ca9e614f2fac7707
Dung Ta Van <itprono3@gmail.com>
2025-05-04 20:49:29 +0700

    move generatePageFullHtml to bottom to help review be easier

--- plugins/preset-webpage/src/methods/index.js
+++ plugins/preset-webpage/src/methods/index.js
@@ -1,27 +1,3 @@
-function generatePageFullHtml(html, css, js) {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Exported Page</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css">
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-      <style>${css}</style>
-    </head>
-    <body>
-      ${html}
-      
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
-      <script>${js}</script>
-    </body>
-    </html>
-  `.trim();
-}
 export default (editor) => {
   editor.getInnerHtml = function () {
     let html = editor.getHtml();
@@ -55,3 +31,28 @@ export default (editor) => {
     return generatePageFullHtml(html, css, js);
   };
 };
+
+function gi(html, css, js) {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Exported Page</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.css">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+      <style>${css}</style>
+    </head>
+    <body>
+      ${html}
+      
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/11.0.5/swiper-bundle.min.js"></script>
+      <script>${js}</script>
+    </body>
+    </html>
+  `.trim();
+}
